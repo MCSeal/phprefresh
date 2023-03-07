8e508588dc7d4f0aadd8d71e9bf647ede4c7aebd
MCSeal <mzeal93@gmail.com>
2023-03-07 18:44:44 -0500

    first commit

---
+++ dynamic.php
@@ -0,0 +1,5 @@
+<?php 
+
+$title= "My Bois Site"
+
+?>
---
+++ embed.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <h1>welcome</h1>
+
+    <?php 
+//title of site
+$title= "My Bois Site"
+
+?>
+    <?php echo$title; ?>
+</head>
+
+<body>
+
+</body>
+
+</html>
